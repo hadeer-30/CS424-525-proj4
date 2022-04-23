4c7553a6cfe53332ba596d857b3849a0a16574b8
hadeer-30 <hfarahat@vols.utk.edu>
2022-04-23 18:52:41 -0400

    generalizing the prediction and initial characters, weights file is a fixed name

--- rnn.py
+++ rnn.py
@@ -49,15 +49,24 @@ def data_division (fname, wsize, stride):
     #encode each character as a number
     chars = sorted(list(set(in_data)))
     mapping = dict((c, i) for i, c in enumerate(chars))
+    
     encoded_line = np.array([mapping[char] for char in in_data])
-    # print("encoded_line shape and data:")
-    # print(encoded_line.shape)
-    # print(encoded_line)
+    #encoded_line = in_data
+
+    print("encoded_line shape and data:")
+    print(encoded_line.shape)
+    print(encoded_line)
+
+
     seq_length = 12
     init_char = []
     for i in range(0, len(in_data) - seq_length, 1):
         seq_in = in_data[i:i + seq_length]
         init_char.append([mapping[char] for char in seq_in])
+    print("init_char shape and data:")
+    print(init_char)
+    print(len(init_char))
+
     data_len = len(encoded_line)
     data_list = []
     for i in range(0, data_len - wsize, stride):
@@ -81,11 +90,12 @@ def data_division (fname, wsize, stride):
     # y_list = np.array(y_list)
 
     vocab_size = len(mapping)
+    print("vocab_size:", vocab_size)
     encoded_x = []
     for x in x_list:
-        print(x)        
+        #print(x)        
         one_hot_line = to_categorical(x, num_classes=vocab_size)
-        print(one_hot_line.shape)
+        #print(one_hot_line.shape)
         encoded_x.append(one_hot_line)
     
     encoded_x = np.array(encoded_x)
@@ -97,6 +107,8 @@ def data_division (fname, wsize, stride):
         encoded_y.append(one_hot_line)
     encoded_y = np.array(encoded_y)
     print("encoded_y shape:", encoded_y.shape)
+
+    init_char = encoded_x[0]
 
     return encoded_x, encoded_y,init_char
 """ 
@@ -111,11 +123,21 @@ def data_division (fname, wsize, stride):
  """
 
 def char_prediction (init_char, model, temp, n,fname):
+    print('init_chars: ',init_char)
+    init_char = init_char.tolist()
+
+    """chars = sorted(list(set(init_char)))
+    mapping = dict((c, i) for i, c in enumerate(chars))    
+    encoded_line = np.array([mapping[char] for char in init_char])
+    """
+
     in_data = read_file(fname)
     chars = sorted(list(set(in_data)))
     int_map = dict((i, c) for i, c in enumerate(chars))
     index = np.random.randint(0, len(init_char)-1)
     pattern = init_char[index]
+    print("pattern:", pattern)
+    pred_seq=""
     print("\nPredicted Sequence:")
     for i in range(n):
         num = np.reshape(pattern, (1, 1, len(pattern)))
@@ -123,9 +145,14 @@ def char_prediction (init_char, model, temp, n,fname):
         pred_num = model.predict(num, verbose=0)
         index = np.argmax(pred_num)
         new_char = int_map[index]
+        pred_seq = pred_seq + new_char
         sys.stdout.write(new_char)
         pattern.append(index)
         pattern = pattern[1:len(pattern)]
+    print("\n")
+    print("Predicted Sequence (string):", pred_seq)
+    print("seq length:", len(pred_seq))
+    return pred_seq
     #write a method that predicts a given number of characters given a certain model and some characters to initialize
 
 
@@ -151,10 +178,13 @@ def model_train(model, y_train,x_train,epochs,lr,decay):
     rmodel.compile(loss='mean_squared_error', optimizer=opt)    
         
         # define the checkpoint
-    filepath="weights-improvement-{epoch:02d}-{loss:.4f}.hdf5"
-    checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
+    #filepath="weights-improvement-{epoch:02d}-{loss:.4f}.hdf5"
+    filepath="weights.hdf5"
+    #checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
+    checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=0, save_best_only=True, mode='min')
         # fit the model
-    rmodel.fit(x_train, y_train, epochs=epochs, batch_size=1, verbose=2,callbacks=[checkpoint])
+    #rmodel.fit(x_train, y_train, epochs=epochs, batch_size=1, verbose=2,callbacks=[checkpoint])
+    rmodel.fit(x_train, y_train, epochs=epochs, batch_size=1, verbose=0,callbacks=[checkpoint])
     rmodel.summary()
     #rmodel.add(layers.Dense(1))
     """ print("From training -- vocab_size:", vocab_size)
@@ -212,7 +242,9 @@ if __name__=="__main__":
     x_train = np.reshape(x_train,(1,x_train.shape[0],x_train.shape[1],x_train.shape[2]))
     #print("AFTER RESHAPE: x_train shape:", x_train.shape)
     #print("rmodel.predict(x_train[0]):", rmodel.predict(x_train[0]))
-    filepath = "weights-improvement-173-0.0630.hdf5"    #the file name of the best weights
+    
+    #filepath = "weights-improvement-200-0.0001.hdf5"    #the file name of the best weights
+    filepath = "weights.hdf5"    #the file name of the best weights
     rmodel.load_weights(filepath)
     opt = keras.optimizers.Adam(learning_rate=lr,decay=decay)
     rmodel.compile(loss='mean_squared_error', optimizer=opt)
